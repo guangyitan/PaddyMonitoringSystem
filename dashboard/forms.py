7e41e04d517ec07dbdafb145067dec75e92b13f5
Guang Yi <guangyitan1999@gmail.com>
2021-12-12 03:07:22 +0800

    respell logitude to longitude

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -7,7 +7,7 @@ from crispy_forms.layout import Submit, Layout, Field
 class NewPaddyAreaInfoForm(forms.Form):
     area_name = forms.CharField(label = "Area Name", max_length = 225, required=True)
     start_date = forms.DateTimeField(label = "Strat Date(DD/MM/YYYY)", required=True)
-    logitude = forms.DecimalField(label = "Logitude (Min -180, Max 180)", max_digits = 9, decimal_places = 6, required=True)
+    longitude = forms.DecimalField(label = "longitude (Min -180, Max 180)", max_digits = 9, decimal_places = 6, required=True)
     latitude = forms.DecimalField(label = "Latitude (Min 0, Max 90)", max_digits = 9, decimal_places = 6, required=True)
 
 class PaddyAreaInfoForm(forms.ModelForm):
@@ -19,6 +19,6 @@ class PaddyAreaInfoForm(forms.ModelForm):
         labels = {
             'area_name': ('Area Name'),
             'start_date': ('Start Date(DD/MM/YYYY)'),
-            'logitude': ('Logitude (Min -180, Max 180)'),
+            'longitude': ('longitude (Min -180, Max 180)'),
             'latitude': ('Latitude (Min 0, Max 90)'),
         }
