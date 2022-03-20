67012b2e0c434f80e39c238946ef9d9b7a8c03c5
Guang Yi <guangyitan1999@gmail.com>
2022-03-20 22:29:34 +0800

    setup env

--- PaddyMonitoringSystem/settings.py
+++ PaddyMonitoringSystem/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import environ
+
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_y6s8=w732%aw7zu^5aja@@$07k1@!+f#-j!k+e28g^2dmu62-'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
